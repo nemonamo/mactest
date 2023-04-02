21303ed22a800f7fa317cf78e83b506a0d76331c
nemonamo <namho0941@daum.net>
2023-04-02 15:07:12 +0900

    hi

--- HW3_NAMHO_KIM/socketcl.py
+++ HW3_NAMHO_KIM/socketcl.py
@@ -1,5 +1,4 @@
 import socket
-import struct
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ('localhost', 9000)
@@ -10,7 +9,8 @@ print(msg.decode())
 sock.sendall(b'Namho Kim')
 
 student_id_bytes = sock.recv(4)
-student_id = struct.unpack('!i', student_id_bytes)[0] # big-endian으로 변환
+student_id = int.from_bytes(student_id_bytes,'big')
 print(student_id)
 
 sock.close()
+
--- HW3_NAMHO_KIM/socketserver.py
+++ HW3_NAMHO_KIM/socketserver.py
@@ -1,7 +1,5 @@
-
 import socket
-import struct
-from http import client
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 9000))
 
@@ -10,13 +8,11 @@ s.listen(2)
 while True:
     client, addr = s.accept()
     print('connection from', addr)
-    # 클라이언트에게 인사 메시지 전송
     client.send(b'hello ' + addr[0].encode())
-    # 학생의 이름 수신
     name = client.recv(1024).decode()
     print(name)
-    # 학생의 학번 전송
-    student_id = 20171510  # 학번 입력
-    student_id_bytes = struct.pack('!i', student_id)  # big-endian으로 변환
+    student_id = 20171510  
+    student_id_bytes = student_id.to_bytes(4,'big')
+    
     client.sendall(student_id_bytes)
     client.close()
